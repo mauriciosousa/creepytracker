6f772e6dec060e2526b1abb3de32815fdfd79c11
danieltmendes <danieltmendes@gmail.com>
2016-02-18 17:34:10 +0000

    Updated Camera Orbit

--- Assets/common/Scripts/CameraOrbit.cs
+++ Assets/common/Scripts/CameraOrbit.cs
@@ -24,8 +24,8 @@ public class CameraOrbit : MonoBehaviour {
         //x = angles.y;
         //y = angles.x;
 
-        if (GetComponent<Rigidbody>())
-            GetComponent<Rigidbody>().freezeRotation = true;
+        /*if (GetComponent<Rigidbody>())
+            GetComponent<Rigidbody>().freezeRotation = true;*/
     }
 
     void LateUpdate()
@@ -46,11 +46,11 @@ public class CameraOrbit : MonoBehaviour {
 			
             distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel") * 5, distanceMin, distanceMax);
 
-            RaycastHit hit;
+            /*RaycastHit hit;
             if (Physics.Linecast(target.position, transform.position, out hit))
             {
                 distance -= hit.distance;
-            }
+            }*/
             Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
             Vector3 position = rotation * negDistance + target.position;
 
